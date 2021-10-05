53bb66bb752ee8107a019807405b4d6f62dbf3fc
Erdet Nasufi <erdet.nasufi@gmail.com>
2021-10-05 20:56:52 +0200

    Added: support for TF_Code (enum Code) and testing code. Also, some C TF_ struct prototypes are added.

---
+++ test_tf_enum_code.v
@@ -0,0 +1,9 @@
+module main
+
+import tf
+
+fn main() {
+	tf_code := tf.Code(6)
+
+	println(tf_code.str())
+}
--- tf/tf.v
+++ tf/tf.v
@@ -13,6 +13,32 @@ pub fn version() string {
 }
 
 ///
+/// Status
+///
+struct C.TF_Status {}
+
+/// TF_Code
+pub enum Code {
+	ok                  = C.TF_OK // [0]
+	cancelled           = C.TF_CANCELLED // [1]
+	unknown             = C.TF_UNKNOWN // [2]
+	invalid_argument    = C.TF_INVALID_ARGUMENT // [3]
+	deadline_exceeded   = C.TF_DEADLINE_EXCEEDED // [4]
+	not_found           = C.TF_NOT_FOUND // [5]
+	already_exists      = C.TF_ALREADY_EXISTS // [6]
+	permission_denied   = C.TF_PERMISSION_DENIED // [7]
+	unaunthenticated    = C.TF_UNAUTHENTICATED // [16]
+	resource_exhausted  = C.TF_RESOURCE_EXHAUSTED // [8]
+	failed_precondition = C.TF_FAILED_PRECONDITION // [9]
+	aborted             = C.TF_ABORTED // [10]
+	out_of_range        = C.TF_OUT_OF_RANGE // [11]
+	unimplemented       = C.TF_UNIMPLEMENTED // [12]
+	internal            = C.TF_INTERNAL // [13]
+	unavaiable          = C.TF_UNAVAILABLE // [14]
+	data_loss           = C.TF_DATA_LOSS // [15]
+}
+
+///
 /// TF_Graph
 ///
 
@@ -31,3 +57,15 @@ pub fn new_graph() Graph {
 pub fn (this Graph)delete() {
 	C.TF_DeleteGraph(this.graph)
 }
+
+///
+/// Input, Output, Function and Operation structs
+///
+
+struct C.TF_OperationDescription{}
+struct C.TF_Operation{}
+struct C.TF_Input{}
+struct C.TF_Output{}
+struct C.TF_Function{}
+struct C.TF_FunctionOptions{}
+
