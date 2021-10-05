884ef5ad11b660399d988bbc70d8afa7d53f9938
Erdet Nasufi <erdet.nasufi@gmail.com>
2021-10-06 00:10:45 +0200

    Adding support for TF_Session (needs to be tested and extending the support). Renaming handler of each struct from <handler> to <handler>_ptr

--- tf/tf.v
+++ tf/tf.v
@@ -5,6 +5,8 @@ module tf
 
 #include <tensorflow/c/c_api.h>
 
+pub const null = voidptr(0)
+
 fn C.TF_Version() &char
 pub fn version() string {
 	unsafe {
@@ -39,7 +41,7 @@ pub enum Code {
 }
 
 pub struct Status {
-	status &C.TF_Status
+	status_ptr &C.TF_Status
 }
 
 fn C.TF_NewStatus() &C.TF_Status
@@ -54,19 +56,19 @@ pub fn new_status() Status {
 }
 
 pub fn (this Status)delete() {
-	C.TF_DeleteStatus(this.status)
+	C.TF_DeleteStatus(this.status_ptr)
 }
 
 pub fn (this Status)set(code Code, message string) {
-	C.TF_SetStatus(this.status, int(code), &char(message.str))
+	C.TF_SetStatus(this.status_ptr, int(code), &char(message.str))
 }
 
 pub fn (this Status)set_from_io_error(error_code int, context string) {
-	C.TF_SetStatusFromIOError(this.status, error_code, &char(context.str))
+	C.TF_SetStatusFromIOError(this.status_ptr, error_code, &char(context.str))
 }
 
 pub fn (this Status)code() Code {
-	rc := C.TF_GetCode(this.status)
+	rc := C.TF_GetCode(this.status_ptr)
 
 	return Code(rc)
 }
@@ -76,7 +78,7 @@ pub fn (this Status)code() Code {
 //          associated with that error.
 pub fn (this Status)message() string {
 	unsafe {
-		msg := &char(C.TF_Message(this.status))
+		msg := &char(C.TF_Message(this.status_ptr))
 		return msg.vstring_literal()
 	}
 }
@@ -92,7 +94,7 @@ fn C.TF_DeleteBuffer(&C.TF_Buffer)
 fn C.TF_GetBuffer(&C.TF_Buffer)C.TF_Buffer
 
 struct Buffer {
-	buffer &C.TF_Buffer
+	buffer_ptr &C.TF_Buffer
 }
 
 pub fn new_buffer() Buffer {
@@ -104,7 +106,7 @@ pub fn new_buffer_from_string(buffer string) Buffer {
 }
 
 pub fn (this Buffer)delete() {
-	C.TF_DeleteBuffer(this.buffer)
+	C.TF_DeleteBuffer(this.buffer_ptr)
 }
 
 ///
@@ -117,7 +119,7 @@ fn C.TF_SetConfig(&C.TF_SessionOptions, voidptr, u64, &C.TF_Status)
 fn C.TF_DeleteSessionOptions(&C.TF_SessionOptions)
 
 pub struct SessionOptions {
-	session_option &C.TF_SessionOptions
+	session_options_ptr &C.TF_SessionOptions
 }
 
 pub fn new_session_options() SessionOptions {
@@ -127,11 +129,11 @@ pub fn new_session_options() SessionOptions {
 }
 
 pub fn (this SessionOptions)set_target(target string) {
-	C.TF_SetTarget(this.session_option, &char(target.str))
+	C.TF_SetTarget(this.session_options_ptr, &char(target.str))
 }
 
 pub fn (this SessionOptions)delete() {
-	C.TF_DeleteSessionOptions(this.session_option)
+	C.TF_DeleteSessionOptions(this.session_options_ptr)
 }
 
 ///
@@ -143,7 +145,7 @@ fn C.TF_NewGraph() &C.TF_Graph
 fn C.TF_DeleteGraph(&C.TF_Graph)
 
 pub struct Graph {
-	graph &C.TF_Graph
+	graph_ptr &C.TF_Graph
 }
 
 pub fn new_graph() Graph {
@@ -151,7 +153,7 @@ pub fn new_graph() Graph {
 }
 
 pub fn (this Graph)delete() {
-	C.TF_DeleteGraph(this.graph)
+	C.TF_DeleteGraph(this.graph_ptr)
 }
 
 ///
@@ -164,4 +166,70 @@ struct C.TF_Input{}
 struct C.TF_Output{}
 struct C.TF_Function{}
 struct C.TF_FunctionOptions{}
+
+///
+/// Session
+///
+struct C.TF_Session{}
+
+fn C.TF_NewSession(&C.TF_Graph, &C.TF_SessionOptions, &C.TF_Status) &C.TF_Session
+// (TF_Graph* graph, const TF_SessionOptions* opts, TF_Status* status)
+
+fn C.TF_LoadSessionFromSavedModel(&C.TF_SessionOptions,
+								  &C.TF_Buffer,
+								  &char, &&char, int
+								  &C.TF_Graph,
+								  &C.TF_Buffer,
+								  &C.TF_Status) &C.TF_Session
+// TF_LoadSessionFromSavedModel(const TF_SessionOptions* session_options, 
+//                              const TF_Buffer* run_options,
+//                              const char* export_dir, 
+//                              const char* const* tags, int tags_len,
+//                              TF_Graph* graph, TF_Buffer* meta_graph_def, 
+//                              TF_Status* status);
+
+fn C.TF_CloseSession(&C.TF_Session, &C.TF_Status)
+
+fn C.TF_DeleteSession(&C.TF_Session, &C.TF_Status)
+
+pub struct Session {
+	session_ptr &C.TF_Session
+}
+
+pub fn new_session(graph &Graph, session_options &SessionOptions, status &Status) Session {
+	return Session {
+		C.TF_NewSession(
+			graph.graph_ptr, 
+			session_options.session_options_ptr,
+			status.status_ptr
+		)
+	}
+}
+
+pub fn load_session_from_model(session_options &SessionOptions,
+							   run_options     &Buffer,
+							   export_dir       string,
+							   tags           &&char,
+							   tags_len        u32,
+							   graph           &Graph,
+							   meta_graph_def  &Buffer,
+							   status          &Status) Session {
+	return Session {C.TF_LoadSessionFromSavedModel(
+		session_options.session_options_ptr,
+		run_options.buffer_ptr,
+		&char(export_dir.str),
+		tags, int(tags_len),
+		graph.graph_ptr,
+		meta_graph_def.buffer_ptr,
+		status.status_ptr)
+	}
+}
+
+pub fn (this Session)close(status &Status) {
+	C.TF_CloseSession(this.session_ptr, status.status_ptr)
+}
+
+pub fn (this Session)delete(status &Status) {
+	C.TF_DeleteSession(this.session_ptr, status.status_ptr)
+}
 
